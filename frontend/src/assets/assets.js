fda89c11b543898459d0ca96afe0645dcd6c141f
Bagus Satrio Wicaksono <jeongiezen2@gmail.com>
2025-03-18 21:19:56 +0700

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,24 @@
+import kwulogo from './kwu_logo.png';
+import ospekkit from './ospek_kit.png';
+
+export const assets = {
+  kwulogo,
+  ospekkit
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'OSPEK Kit',
+    price: 120000,
+    image: 'ospekkit',
+    description: 'Kit OSPEK yang berisi berbagai perlengkapan esensial untuk OSPEK Fasilkom 2025'
+  },
+  {
+    id: 2,
+    name: 'Kaos Fasilkom',
+    price: 50000,
+    image: 'kaos1',
+    description: 'kaos Fasilkom yang menandakan kamu adalah bagian dari KM Fasilkom UPN Veteran Jawa Timur'
+  }
+]
