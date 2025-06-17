835ba8c40520a86cbb1966ea047dc266a02beca1
Bagus Satrio Wicaksono <jeongiezen2@gmail.com>
2025-06-17 13:50:24 +0700

    add hero products, and update file image multer routes.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,8 @@ import {
   getProduct,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  getHeroProducts
 } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
 import { auth, requireAdmin } from '../middleware/authMiddleware.js';
@@ -13,6 +14,7 @@ const router = express.Router();
 
 // Public routes
 router.get('/', getAllProducts);
+router.get('/hero', getHeroProducts);
 router.get('/:id', getProduct);
 
 // Admin routes
@@ -22,7 +24,12 @@ router.post('/create', upload.fields([
                         {name:'image3', maxCount:1}, 
                         {name:'image4', maxCount:1},
                       ]), auth, requireAdmin, createProduct); 
-router.put('/update/:id', auth, requireAdmin, updateProduct);
+router.put('/update/:id', upload.fields([
+                        {name:'newImage1', maxCount:1}, 
+                        {name:'newImage2', maxCount:1}, 
+                        {name:'newImage3', maxCount:1}, 
+                        {name:'newImage4', maxCount:1},
+                      ]), auth, requireAdmin, updateProduct);
 router.delete('/delete/:id', auth, requireAdmin, deleteProduct);
 
 export default router; 
